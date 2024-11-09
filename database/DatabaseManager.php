d12fd2e22f7f2874b4174d6fd0e6e722458a5d1b
grintzdel <mathis.oudin@etu.uca.fr>
2024-11-09 16:49:08 +0100

    Code's architecture

---
+++ database/DatabaseManager.php
@@ -0,0 +1,95 @@
+<?php
+
+require_once 'config.php';
+
+class DatabaseManager
+{
+    protected static $instance;
+    public $pdo;
+
+    /**
+     * @return DatabaseManager
+     *
+     * Function for create a new instance of DatabaseManager if not exist
+     */
+    public static function getInstance(): DatabaseManager
+    {
+        if (!self::$instance) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    private function __construct()
+    {
+        $this->connect();
+    }
+
+
+    /**
+     * @return void
+     *
+     * Function for connect to the database
+     */
+    private function connect()
+    {
+        try {
+            $this->pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASSWORD);
+        } catch (PDOException $e) {
+            die("Erreur de connexion à la base de données ! " . $e->getMessage());
+        }
+    }
+
+    /**
+     * @param string $request
+     * @param array $param
+     * @param string $pdoOptions
+     *
+     * @return array
+     *
+     * Function for create a request to the database and return the result
+     */
+    public function select(
+        string $request,
+        array  $param = [],
+        string $pdoOptions = PDO::FETCH_ASSOC
+    ): array
+    {
+        try {
+            $query = $this->pdo->prepare($request);
+            $query->execute($param);
+            return $query->fetchAll($pdoOptions);
+        } catch (PDOException $e) {
+            die("La requête SQL à échoué: " . $e->getMessage());
+        }
+
+    }
+
+    /**
+     * @param string $request
+     * @param array $param
+     * @return void
+     *
+     * Function for insert data in the database
+     */
+    public function insert(
+        string $request,
+        array  $param = []
+    ): void
+    {
+        try {
+            $query = $this->pdo->prepare($request);
+            $query->execute($param);
+        } catch (PDOException $e) {
+            die("La requête SQL à échoué: " . $e->getMessage());
+        }
+    }
+
+    /**
+     * @return int
+     */
+    public function lastInsertId(): int
+    {
+        return $this->pdo->lastInsertId();
+    }
+}
---
+++ database/config.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+define('DB_HOST', getenv('DB_HOST'));
+define('DB_NAME', getenv('DB_NAME'));
+define('DB_USER', getenv('DB_USER'));
+define('DB_PASSWORD', getenv('DB_PASSWORD'));
