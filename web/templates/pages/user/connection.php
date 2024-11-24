c891ad5525ae2ad495261ed7c04ea7eeaa68ebff
grintzdel <mathis.oudin@etu.uca.fr>
2024-11-24 13:57:53 +0100

    💄 Added auth form style

--- web/templates/pages/user/connection.php
+++ web/templates/pages/user/connection.php
@@ -1,26 +1,33 @@
-<?php
-ob_start();
-/**
- * @var string $username Username write in the form
- * @var string $error    Error message if the username or password is incorrect
- */
-?>
+<!DOCTYPE html>
+<html lang="fr">
+<?php require_once 'templates/partials/head.php'; ?>
+<body>
+<?php require_once 'templates/partials/header.php'; ?>
+<main>
+<section class="auth">
+    <div class="auth__container">
+        <div class="auth__img">
+            <img src="/views/img/form__img.png" alt="IUT Chambéry">
+        </div>
+        <div class="auth__form">
+            <h1 class="auth__form__title">Content de te revoir !</h1>
+            <form action="/connexion" method="post">
+                <label for="username">Nom d'utilisateur</label>
+                <input type="text" name="username" id="username" placeholder="Entre ton nom d'utilisateur" value="<?= $username ?>" required>
 
-<h1 class="title">Connexion</h1>
-<form action="/connexion" method="post">
-    <label for="username">Nom d'utilisateur</label>
-    <input type="text" name="username" id="username" placeholder="Nom d'utilisateur" value="<?= $username ?>" required>
+                <label for="password">Mot de passe</label>
+                <input type="password" name="password" id="password" placeholder="Entre ton mot de passe" required>
 
-    <label for="password">Mot de passe</label>
-    <input type="password" name="password" id="password" placeholder="Mot de passe" required>
+                <input class="button button--primary" type="submit" name="login" value="Se connecter">
+            </form>
+        </div>
+    </div>
+</section>
 
-    <input class="button" type="submit" name="login" value="Se connecter">
-</form>
 <?php if (isset($error)): ?>
     <p class="error"><?= $error ?></p>
 <?php endif; ?>
 
-<?php
-$content = ob_get_clean();
-require_once __DIR__ . '/../base.php';
-?>
+<?php require_once 'templates/partials/footer.php'; ?>
+</body>
+</html>
