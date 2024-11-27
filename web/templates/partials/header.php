04faf04d8ae9b9e57e3b91b50321094f36d322de
grintzdel <mathis.oudin@etu.uca.fr>
2024-11-27 22:57:12 +0100

    💄 Ui

--- web/templates/partials/header.php
+++ web/templates/partials/header.php
@@ -10,37 +10,19 @@
                 <div class="header__nav__menu__list__items">
                     <div class="header__nav__menu__list__items__item">
                         <a href="/">How ?</a>
+                        <div class="header__nav__menu__list__items__item__wrapper"></div>
                     </div>
                     <div class="header__nav__menu__list__items__item">
                         <a href="/profil">Profil</a>
+                        <div class="header__nav__menu__list__items__item__wrapper"></div>
                     </div>
                     <div class="header__nav__menu__list__items__item">
-                        <a href="/">Panier</a>
+                        <a href="/panier">Panier</a>
+                        <div class="header__nav__menu__list__items__item__wrapper"></div>
                     </div>
                     <div class="header__nav__menu__list__items__item">
-                        <details class="header__nav__menu__details" open>
-                            <summary class="header__nav__menu__details__heading">
-                                <a class="header__nav__menu__details__heading__title">Equipements</a>
-                                <img src="/views/svg/open-close--white.svg" alt="Toogle menu">
-                            </summary>
-                            <ul class="header__nav__menu__details__list">
-                                <li><a href="/">Tout</a></li>
-                                <li><a href="/">Appareils photo</a></li>
-                                <li><a href="/">Caméras</a></li>
-                                <li><a href="/">Micros</a></li>
-                                <li><a href="/">Enregistreurs</a></li>
-                                <li><a href="/">Câbles</a></li>
-                                <li><a href="/">Adaptateurs</a></li>
-                                <li><a href="/">Casques</a></li>
-                                <li><a href="/">Trépieds</a></li>
-                                <li><a href="/">Eclairages</a></li>
-                                <li><a href="/">Perches</a></li>
-                                <li><a href="/">Moniteurs</a></li>
-                                <li><a href="/">Objectifs photo</a></li>
-                                <li><a href="/">Ordinateurs</a></li>
-                                <li><a href="/">Autres</a></li>
-                            </ul>
-                        </details>
+                        <a href="/equipment">Equipements</a>
+                        <div class="header__nav__menu__list__items__item__wrapper"></div>
                     </div>
                 </div>
             </div>
